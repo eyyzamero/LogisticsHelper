d280a1f2536a8c9f7b056f412a6fa3762ce244a7
eyyzamero <eazymen789@gmail.com>
2022-11-06 13:51:43 +0100

    AssignmentForm initial logic to get assignment data

--- src/app/components/assignments/components/form/assignment-form.component.ts
+++ src/app/components/assignments/components/form/assignment-form.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AssignmentModel, IAssignmentModel } from '../../models';
+import { AssignmentsObservableService } from '../../services/observable/assignments-observable.service';
 
 @Component({
   selector: 'app-assignment-form',
   templateUrl: './assignment-form.component.html',
   styleUrls: ['./assignment-form.component.scss'],
 })
-export class AssignmentFormComponent {
+export class AssignmentFormComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  assignment: IAssignmentModel = new AssignmentModel();
+
+  private _assignmentId: string | null = null;
+  private _subscriptions: Array<Subscription> = new Array<Subscription>();
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _assignmentsObservableService: AssignmentsObservableService
+  ) { }
+
+  ngOnInit(): void {
+    this._initObservables();
+  }
+
+  private _initObservables(): void {
+    const routeParamsSubscription = this._activatedRoute.paramMap.subscribe({
+      next: (paramMap) => this._assignmentId = paramMap.get('id')
+    });
+    const assignmentsSubscription = this._assignmentsObservableService.observable.subscribe({
+      next: (value) => {
+        if (this._assignmentId) 
+          this._checkIfAssignmentIsInObservable(value.data);
+      }
+    });
+    this._subscriptions.push(routeParamsSubscription, assignmentsSubscription);
+  }
+
+  private _checkIfAssignmentIsInObservable(assignments: Array<IAssignmentModel>): void {
+    const assignment = assignments.find(x => x.id === this._assignmentId);
+
+    if (assignment)
+      this.assignment = assignment;
+    else
+      this._getAssignment();
+  }
+
+  private async _getAssignment(): Promise<void> {
+
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.forEach(x => x.unsubscribe());
+  }
 }

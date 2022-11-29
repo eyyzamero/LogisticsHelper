fd03bf11172f90f768cf165d6e4ea938677a13aa
eyyzamero <eazymen789@gmail.com>
2022-11-29 21:21:21 +0100

    Added capacitor config through "ionic init" command

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'eyyzamero.logistics.helper',
+  appName: 'LogisticsHelper',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

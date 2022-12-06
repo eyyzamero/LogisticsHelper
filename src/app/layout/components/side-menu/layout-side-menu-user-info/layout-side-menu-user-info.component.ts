27a949d588e95621416d6f026a9aaf1c875666ce
eyyzamero <eazymen789@gmail.com>
2022-12-07 00:51:51 +0100

    Console log removed

--- src/app/layout/components/side-menu/layout-side-menu-user-info/layout-side-menu-user-info.component.ts
+++ src/app/layout/components/side-menu/layout-side-menu-user-info/layout-side-menu-user-info.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { IAuthModel } from 'src/app/core/models';
 import { AuthObservableService } from 'src/app/core/services/observable/auth/auth-observable.service';
 
@@ -11,7 +11,6 @@ import { AuthObservableService } from 'src/app/core/services/observable/auth/aut
 export class LayoutSideMenuUserInfoComponent {
 
   user$: Observable<IAuthModel | null> = this._authObservableService.observable.pipe(
-    tap(x => console.log(x)),
     map(x => x.data)
   );
 
